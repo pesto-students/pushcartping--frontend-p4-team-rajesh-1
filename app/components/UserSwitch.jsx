695d037249250ca450b3ba9977a3446e2be7f58c
mr-rego <mihir.rego@outlook.com>
2023-01-14 15:26:16 +0530

    Optimise UserSwitch

--- app/components/UserSwitch.jsx
+++ app/components/UserSwitch.jsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View, Switch } from 'react-native'
-import React, { useState, useContext, useEffect } from 'react'
+import { StyleSheet, View } from 'react-native'
+import React, { useContext, useEffect } from 'react'
 import SwitchSelector from 'react-native-switch-selector'
 
 import constants from '../config/constants';
@@ -7,22 +7,17 @@ import { UserContext } from '../context/UserContext';
 
 const UserSwitch = () => {
     const { userType, setUserType } = useContext(UserContext);
-    const [isON, setIsON] = useState(true);
-
-    const toggleSwitch = () => {
-        setIsON(previousState => !previousState);
-    }
 
     useEffect(() => {
-        isON ? setUserType(constants.userTypeVendor) : setUserType(constants.userTypeCustomer)
-    }, [isON]);
+        setUserType(constants.userTypeCustomer)
+    }, []);
 
     return (
         <View style={styles.container}>
             <SwitchSelector
                 initial={constants.userTypeCustomer}
-                onPress={toggleSwitch}
-                textColor={constants.colorBlack} //'#7a44cf'
+                onPress={value => setUserType(value)}
+                textColor={constants.colorBlack}
                 selectedColor={constants.colorWhite}
                 buttonColor={constants.colorButton}
                 borderColor={constants.colorBackground}
